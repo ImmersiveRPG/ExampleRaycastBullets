52ec898300a190d019ed4027aeab69d22a72eab0
Matthew Brennan Jones <matthew.brennan.jones@gmail.com>
2021-11-12 23:33:49 -0800

    Made steel block rotate randomly

--- project/src/SteelBlock/SteelBlock.gd
+++ project/src/SteelBlock/SteelBlock.gd
@@ -7,7 +7,17 @@ extends RigidBody
 signal apply_force(angle, force)
 
 var _element : int = Global.Element.Steel
+var _rand_rotation_force := Vector3.ZERO
 
 func _on_apply_force(angle : Vector3, force : float) -> void:
-	#print(force)
 	self.apply_central_impulse(force * angle)
+
+func _process(delta : float) -> void:
+	self.rotation_degrees += _rand_rotation_force * delta
+
+
+func _on_timeout() -> void:
+	var power := 200.0
+	_rand_rotation_force.x = Global._rng.randf_range(-power, power)
+	_rand_rotation_force.y = Global._rng.randf_range(-power, power)
+	_rand_rotation_force.z = Global._rng.randf_range(-power, power)
